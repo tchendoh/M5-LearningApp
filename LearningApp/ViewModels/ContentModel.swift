ef1f741723232cb604a6f2d943b4c1c15faaa6bf
tchendoh <42325308+tchendoh@users.noreply.github.com>
2022-11-25 10:21:37 -0500

    - organize files into folders - set ContentModel as EnvironmentObject

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Eric Chandonnet on 2022-11-25.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
