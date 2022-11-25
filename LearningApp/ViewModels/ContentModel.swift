c403dc22e68f06827e2e55f133e3411c45d0c18f
tchendoh <42325308+tchendoh@users.noreply.github.com>
2022-11-25 11:23:58 -0500

    - Aller cherche le local data, autant le json que le fichier style

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,6 +9,53 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
     
+    var styleData: Data?
     
+    init () {
+        
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // Aller chercher un url au fichier json
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        
+        do {
+            // Lire le fichier dans un data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            // Essayer de décoder l'object
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assigner ces modules parsés à ContentModel comme propriété @Published
+            self.modules = modules
+        }
+        catch {
+            // Catch l'erreur
+            print("Couldn't parse local json data.")
+        }
+
+        // Aller chercher un url au fichier json
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        
+        do {
+            // Lire le fichier dans un data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            // Assigner ces modules parsés à ContentModel comme propriété @Published
+            self.styleData = styleData
+        }
+        catch {
+            // Catch l'erreur
+            print("Couldn't parse local style data.")
+        }
+
+        
+    }
 }
